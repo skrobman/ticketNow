c2589e16d8af3f8e835bbade949332666b02858c
Daniil Shostsik 248680 <248680@edu.p.lodz.pl>
2025-07-20 23:12:32 +0200

    ABSOLUTELY NEW FUNCTIONALITY

--- src/main/java/skrobman/dev/springstart/common/validation/StrongPasswordValidator.java
+++ src/main/java/skrobman/dev/springstart/common/validation/StrongPasswordValidator.java
@@ -5,7 +5,7 @@ import jakarta.validation.ConstraintValidatorContext;
 import skrobman.dev.springstart.common.annotation.StrongPassword;
 
 public class StrongPasswordValidator implements ConstraintValidator<StrongPassword, String> {
-    private static final String Password_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{12,}$";
+    private static final String Password_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$!%^&+=])(?=\\S+$).{12,}$";
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context){
